5ade48e53686d132d719801a519b4f13308bf6d3
ViniciusTonholi <162928911+ViniciusTonholi@users.noreply.github.com>
2024-12-02 21:53:27 -0300

    Delete Backend directory

--- Backend/routes.transactions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addTransaction, getTransactions, deleteTransaction } = require('../controllers/transactionController');
-
-router.post('/', addTransaction);
-router.get('/', getTransactions);
-router.delete('/:id', deleteTransaction);
-
-module.exports = router;
